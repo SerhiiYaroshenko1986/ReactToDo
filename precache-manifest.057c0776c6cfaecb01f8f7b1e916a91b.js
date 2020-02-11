f69b378c43ac1ece5a41088f97496187443af1ea
Serhii Yaroshenko <datextdk@gmail.com>
2020-02-11 16:46:01 +0200

    first commit

---
+++ precache-manifest.057c0776c6cfaecb01f8f7b1e916a91b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81d7d9c13343f0bffad2d08fe78e352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e26dd866c583dc0f743",
+    "url": "/static/css/main.154d98c8.chunk.css"
+  },
+  {
+    "revision": "2e1a396c118f216cb34a",
+    "url": "/static/js/2.12670d05.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.12670d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e26dd866c583dc0f743",
+    "url": "/static/js/main.a07443d3.chunk.js"
+  },
+  {
+    "revision": "0278e8ea0025aaa51c1d",
+    "url": "/static/js/runtime-main.24ff8315.js"
+  }
+]);
